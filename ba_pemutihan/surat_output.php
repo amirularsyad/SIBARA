7458f5295c089180ef88cd1c5edd1ddd3851fa34
Amirul Arsyad <sharkingfisher12@gmail.com>
2026-04-17 11:53:57 +0700

    Revisi Surat Output BA Pemutihan

--- ba_pemutihan/surat_output.php
+++ ba_pemutihan/surat_output.php
@@ -56,22 +56,51 @@
         }
         .wrapper-ttd tr td{
             border: 1px solid black;
-        }
-        .wrapper-ttd tr td{
             width: 100px;
+            
         }
         .wrapper-ttd tr td img{
-            width: 100%;
+            width: 42pt;
         }
         .wrapper-ttd thead td h1,
-        .wrapper-ttd tbody td p
+        .wrapper-ttd tbody td h1
         {
             font-size: 6pt;
             margin: 0;
             padding: 0;
+            font-weight: bold;
         }
-        .wrapper-ttd thead td h1
-        {font-weight: bold;}
+        .wrapper-paraf{
+            width: 10%;
+            height: max-content;
+            margin: 0;
+            padding: 0;
+        }
+        .wrapper-paraf tr td{
+            display: flex;
+            justify-content: end;
+        }
+        .wrapper-paraf tr td img{
+            width: 38pt;
+        }
+        .wrapper-img{
+            width: 100%;
+            height: max-content;
+            margin: 0;
+            padding: 0;
+            text-align: center;
+        }
+        /* .wrapper-img tr td{
+            border: 1px solid red;
+        } */
+        .wrapper-img tr td img{
+            max-width: 120pt;
+        }
+        .wrapper-img tr td p{
+            margin: 0;
+            padding: 0;
+            font-size: small;
+        }
 
     </style>
 </head>
@@ -151,23 +180,93 @@
     </table>
 
     <div class="w-100 d-flex justify-content-end">
+        <table class="wrapper-paraf">
+            <tr>
+                <td><img src="../assets/database-gambar/1772865264_1_berita_acara_kerusakan-autograph_3.png" alt="ttd-approver-1"></td>
+            </tr>
+            <tr>
+                <td><img src="../assets/database-gambar/1772865264_1_berita_acara_kerusakan-autograph_3.png" alt="ttd-approver-2"></td>
+            </tr>
+        </table>
         <table class="wrapper-ttd text-center">
             <thead>
                 <tr>
-                    <td colspan="5"><h1>Pabrik (Pemohon)</h1></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                    <td colspan="5"><h1>Pabrik (Pemohon)</h1></td><td colspan="9"><h1>Head Office (HO)</h1></td>
                 </tr>
                 <tr>
-                    <td><h1>Dibuat Oleh</h1></td><td colspan="3"><h1>Diperiksa Oleh</h1></td><td><h1>Disetujui Oleh</h1></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                    <td><h1>Dibuat Oleh</h1></td><td colspan="3"><h1>Diperiksa Oleh</h1></td><td><h1>Disetujui Oleh</h1></td><td colspan="4"><h1>Diperiksa Oleh</h1></td><td colspan="5"><h1>Disetujui Oleh</h1></td>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td><img src="../assets/database-gambar/1772865264_1_berita_acara_kerusakan-autograph_3.png" alt="ttd-approver-1"></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                    <td><img src="../assets/database-gambar/1772865264_1_berita_acara_kerusakan-autograph_3.png" alt="ttd-approver-3"></td>
+                    <td><img src="../assets/database-gambar/1772865264_1_berita_acara_kerusakan-autograph_3.png" alt="ttd-approver-4"></td>
+                    <td><img src="../assets/database-gambar/1772865264_1_berita_acara_kerusakan-autograph_3.png" alt="ttd-approver-5"></td>
+                    <td><img src="../assets/database-gambar/1772865264_1_berita_acara_kerusakan-autograph_3.png" alt="ttd-approver-6"></td>
+                    <td><img src="../assets/database-gambar/1772865264_1_berita_acara_kerusakan-autograph_3.png" alt="ttd-approver-7"></td>
+                    <td><img src="../assets/database-gambar/1772865264_1_berita_acara_kerusakan-autograph_3.png" alt="ttd-approver-8"></td>
+                    <td><img src="../assets/database-gambar/1772865264_1_berita_acara_kerusakan-autograph_3.png" alt="ttd-approver-9"></td>
+                    <td><img src="../assets/database-gambar/1772865264_1_berita_acara_kerusakan-autograph_3.png" alt="ttd-approver-10"></td>
+                    <td><img src="../assets/database-gambar/1772865264_1_berita_acara_kerusakan-autograph_3.png" alt="ttd-approver-11"></td>
+                    <td><img src="../assets/database-gambar/1772865264_1_berita_acara_kerusakan-autograph_3.png" alt="ttd-approver-12"></td>
+                    <td><img src="../assets/database-gambar/1772865264_1_berita_acara_kerusakan-autograph_3.png" alt="ttd-approver-13"></td>
+                    <td><img src="../assets/database-gambar/1772865264_1_berita_acara_kerusakan-autograph_3.png" alt="ttd-approver-14"></td>
+                    <td><img src="../assets/database-gambar/1772865264_1_berita_acara_kerusakan-autograph_3.png" alt="ttd-approver-15"></td>
+                    <td><img src="../assets/database-gambar/1772865264_1_berita_acara_kerusakan-autograph_3.png" alt="ttd-approver-16"></td>
                 </tr>
                 <tr>
-                    <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                    <td><h1>Kepala Mill</h1></td>
+                    <td><h1>KTU</h1></td>
+                    <td><h1>AMC</h1></td>
+                    <td><h1>Group Manager</h1></td>
+                    <td><h1>Vice President</h1></td>
+                    <td><h1>AVP Engineering</h1></td>
+                    <td><h1>Dept HR Ops</h1></td>
+                    <td><h1>Dept HRD</h1></td>
+                    <td><h1>Dept Accounting</h1></td>
+                    <td><h1>Dir Operation</h1></td>
+                    <td><h1>Dir Finance</h1></td>
+                    <td><h1>Dir HR</h1></td>
+                    <td><h1>Vice CEO</h1></td>
+                    <td><h1>CEO</h1></td>
                 </tr>
             </tbody>
+        </table>
+    </div>
+    
+    <div class="mt-3 d-flex flex-column align-items-center">
+        <h1 class="custom-fs-10 fw-bold">LAMPIRAN</h1>
+        <table class="wrapper-img">
+            <tr>
+                <td><img src="../assets/img/keyboard-laptop-keyboard-komputer-keyboard-adalah-harga-keyboard-murah-2-1-1.jpg" alt="img-lampiran-1"></td>
+                <td><img src="../assets/img/fajarbatre.png" alt="img-lampiran-2"></td>
+                <td><img src="../assets/img/Lenovo_ThinkPad_X1_Ultrabook_(Nov_16,_2012).png" alt="img-lampiran-3"></td>
+            </tr>
+            <tr>
+                <td><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p></td>
+                <td><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p></td>
+                <td><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p></td>
+            </tr>
+            <tr>
+                <td><img src="../assets/img/Lenovo_ThinkPad_X1_Ultrabook_(Nov_16,_2012).png" alt="img-lampiran-4"></td>
+                <td><img src="../assets/img/keyboard-laptop-keyboard-komputer-keyboard-adalah-harga-keyboard-murah-2-1-1.jpg" alt="img-lampiran-5"></td>
+                <td><img src="../assets/img/fajarbatre.png" alt="img-lampiran-6"></td>
+            </tr>
+            <tr>
+                <td><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p></td>
+                <td><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p></td>
+                <td><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p></td>
+            </tr>
+            <tr>
+                <td><img src="../assets/img/fajarbatre.png" alt="img-lampiran-7"></td>
+                <td><img src="../assets/img/Lenovo_ThinkPad_X1_Ultrabook_(Nov_16,_2012).png" alt="img-lampiran-8"></td>
+                <td><img src="../assets/img/keyboard-laptop-keyboard-komputer-keyboard-adalah-harga-keyboard-murah-2-1-1.jpg" alt="img-lampiran-9"></td>
+            </tr>
+            <tr>
+                <td><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p></td>
+                <td><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p></td>
+                <td><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p></td>
+            </tr>
         </table>
     </div>
 
